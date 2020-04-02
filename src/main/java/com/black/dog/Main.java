2b3174fd998fafeccbc8193873ce63a1e8c96933
tianshancaoyuan <tianshancaoyuanddd@163.com>
2020-04-02 20:30:38 +0800

    【修改说明】上传测试代码。

---
+++ src/main/java/com/black/dog/Main.java
@@ -0,0 +1,15 @@
+package com.black.dog;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
