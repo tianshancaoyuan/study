2ba5c440ca3484c7a503f1e2d49ee48325453018
tianshancaoyuan <tianshancaoyuanddd@163.com>
2020-04-03 14:45:48 +0800

    【修改说明】测试上传是否成功；

--- src/main/java/com/black/dog/Main.java
+++ src/main/java/com/black/dog/Main.java
@@ -3,13 +3,28 @@ package com.black.dog;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = null;
         while ((line = br.readLine()) != null) {
-            System.out.println(line);
+            String[] strs = line.split(" ");
+
+            int i = 0;
+            int[] array = new int[strs.length];
+            for (String s :strs) {
+                array[i++] = Integer.parseInt(s);
+            }
+
+            for (i = 0; i < array.length; i++) {
+                System.out.print(array[i] + " ");
+            }
+            System.out.println();
         }
     }
 }
